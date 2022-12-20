f9b14a0cf48f4f40627330351a96dfde85f7f85a
Shubhaada Sakale <120009475+ruta27@users.noreply.github.com>
2022-12-20 10:31:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,81 +1,65 @@
 
 
-	// first method
-	document.getElementById("weatherSearch").addEventListener('click',function(e){
+var myModal = new bootstrap.Modal(document.getElementById('Modal'), {});
 
-		e.preventDefault();	
+// first method
+document.getElementById("weatherSearch").addEventListener('click', function (e) {
 
-		var city = e.path[1][0].value;
+	e.preventDefault();
 
-		console.log(city);
+	var city = e.path[1][0].value;
 
-		weather.innerHTML =  city;
+	console.log(city);
 
+	weather.innerHTML = city;
 
-
-
-
-		// requesting for data using rapid API
+	// requesting for data using rapid API
 
 
-		const options = {
-			method: 'GET',
-			headers: {
-				'X-RapidAPI-Key': '18608b161cmsh434caee79c4219ep1dc0e8jsnb93a5bfcd0e8',
-				'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
-			}
-		};
-		
-		fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city='+ city, options)
-			.then(response => response.json())
-			.then(response => {
-				max_temp.innerHTML = response.max_temp;
-				min_temp.innerHTML = response.min_temp;
-				temprature.innerHTML = response.temp;
-				humidity_pct.innerHTML = response.humidity;
-				cloudpct.innerHTML = response.cloud_pct;
-				windspeed.innerHTML = response.wind_speed;
-				winddegree.innerHTML = response.wind_degrees;
+	var options = {
+		method: 'GET',
+		headers: {
+			'X-RapidAPI-Key': '18608b161cmsh434caee79c4219ep1dc0e8jsnb93a5bfcd0e8',
+			'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
+		}
+	};
 
+	fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=' + city, options).then(function (response) {
+		return response.json();
+	}).then(function (response) {
+		max_temp.innerHTML = response.max_temp;
+		min_temp.innerHTML = response.min_temp;
+		temprature.innerHTML = response.temp;
+		humidity_pct.innerHTML = response.humidity;
+		cloudpct.innerHTML = response.cloud_pct;
+		windspeed.innerHTML = response.wind_speed;
+		winddegree.innerHTML = response.wind_degrees;
 
-			})
-			.catch(err => console.error(err));
+		if ("error" in response) {
+			console.log("yes error is there");
 
-	
+			myModal.toggle();
+		} else {
+			dynamicrows(response, city);
+		}
+	}).catch(function (err) {
+		console.error(err);
 	});
+});
 
+function dynamicrows(response, city) {
 
+	var table_El = document.querySelector('tbody');
 
+	table_El.innerHTML += '<tr>\n\t<td><b>' + city + '</b></td>\n\t<td>' + response.cloud_pct + '</td>\n\t<td>' + response.feels_like + '</td>\n\t<td>' + response.humidity + '</td>\n\t<td>' + response.max_temp + '</td>\n\t<td>' + response.min_temp + '</td>\n\t<td>' + response.sunrise + '</td>\n\t<td>' + response.sunset + '</td>\n\t<td>' + response.temp + '</td>\n\t<td>' + response.wind_degrees + '</td>\n\t<td>' + response.wind_speed + '</td>\n\t</tr>';
 
+	console.log(response);
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	//second tarika
-	document.getElementById("weatherSearch").onclick = function(t){
-		t.preventDefault();
-		
-	}
-
-
-
-
-
-
-
+//second tarika
+document.getElementById("weatherSearch").onclick = function (t) {
+	t.preventDefault();
+};
 
 
 
